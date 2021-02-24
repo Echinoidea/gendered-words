aa0b73ac84d0ddde8fa6b9ae37673589efb933f5
Gabriel Hooks <gabriel.i.hooks@gmail.com>
2021-02-24 14:28:40 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Gendered Words
+Search through the text from a given URL(s) and find "gendered" words. E.g., female->lipstick.
+Although lipstick is not inherently a female word or item, it is generally associated with women.
+
+If the program finds a gendered word, return the sentence in which it is in.
+
+@author Gabriel Hooks
+@date 2021-02-24
+"""
+
+
+
